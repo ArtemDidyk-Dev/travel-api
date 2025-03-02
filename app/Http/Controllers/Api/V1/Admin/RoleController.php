6a941fbf8136baa0d34d65475d68a4dd7a2570ea
Artem <ad@digiants.com.ua>
2025-03-02 20:43:15 +0200

    add documentation  admin roles

--- app/Http/Controllers/Api/V1/Admin/RoleController.php
+++ app/Http/Controllers/Api/V1/Admin/RoleController.php
@@ -12,6 +12,7 @@ use App\Models\User;
 use App\Services\RoleServicesInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use OpenApi\Attributes as OA;
 
 class RoleController extends Controller
 {
@@ -20,21 +21,166 @@ class RoleController extends Controller
     ) {
     }
 
+    #[OA\Get(
+        path: '/api/v1/admin/roles',
+        description: 'Retrieve a list of all roles. Only accessible to users with Admin or Editor roles.',
+        summary: 'Get all roles',
+        security: [['sanctum' => []]],
+        tags: ['Admin']
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Successfully retrieved list of users',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(
+                        properties: [
+                            new OA\Property(property: 'id', type: 'integer', example: 1),
+                            new OA\Property(property: 'name', type: 'string', example: 'ADMIN'),
+                        ],
+                        type: 'object'
+                    ),
+                    example: [
+                        [
+                            'id' => 1,
+                            'name' => 'ADMIN',
+                        ],
+                        [
+                            'id' => 2,
+                            'name' => 'EDITOR',
+                        ],
+                        [
+                            'id' => 3,
+                            'name' => 'USER',
+                        ],
+                    ]
+
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Unauthenticated',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'message', type: 'string', example: 'Unauthenticated.'),
+            ]
+        )
+    )]
     public function index(): AnonymousResourceCollection
     {
         return RoleResource::collection(Role::all());
     }
 
+
+    #[OA\Post(
+        path: '/api/v1/admin/roles/users/{user}/add',
+        description: 'Assign roles to a user. Only accessible to Admins.',
+        summary: 'Assign roles to a user',
+        security: [['sanctum' => []]],
+        tags: ['Admin']
+    )]
+    #[OA\Parameter(
+        name: 'user',
+        description: 'User ID to whom roles will be assigned',
+        in: 'path',
+        required: true,
+        example: 12
+    )]
+    #[OA\RequestBody(
+        description: 'Roles assignment payload',
+        required: true,
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'roles',
+                    description: 'Array of role IDs to assign',
+                    type: 'array',
+                    items: new OA\Items(type: 'integer', example: 3)
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 201,
+        description: 'Roles assigned successfully',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'message', type: 'string', example: 'Roles assigned successfully.'),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'User already has the selected role',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'message',
+                    type: 'string',
+                    example: 'The user already has the selected role: ADMIN'
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Unauthenticated',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'message', type: 'string', example: 'Unauthenticated.'),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 422,
+        description: 'Validation errors',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'success', type: 'boolean', example: false),
+                new OA\Property(property: 'message', type: 'string', example: 'Validation errors'),
+                new OA\Property(
+                    property: 'errors',
+                    type: 'object',
+                    example: [
+                       'roles' => [
+                           'The selected roles are invalid.'
+                       ]
+                    ]
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 500,
+        description: 'Server error while assigning roles',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'message', type: 'string', example: 'An error occurred while assigning roles.'
+                ),
+                new OA\Property(
+                    property: 'error',
+                    type: 'string',
+                    example: 'SQLSTATE[23000]: Integrity constraint violation...'
+                ),
+            ]
+        )
+    )]
     public function assignRolesToUser(User $user, UserRoles $request): JsonResponse
     {
         try {
             $data = $request->validated();
             $user->load('roles');
-            if (! $this->roleServices->assignRolesToUser(user: $user, data: $data)) {
+            if (!$this->roleServices->assignRolesToUser(user: $user, data: $data)) {
                 $roles = $this->roleServices->getRolesNames(user: $user, roles: $data['roles']);
 
                 return response()->json([
-                    'message' => 'The user already has the selected role: ' . $roles,
+                    'message' => 'The user already has the selected role: '.$roles,
                 ], 400);
             }
 
@@ -49,6 +195,76 @@ class RoleController extends Controller
         }
     }
 
+    #[OA\Delete(
+        path: '/api/v1/admin/roles/users/{user}/delete',
+        description: 'Remove roles from a user. Only accessible to Admins.',
+        summary: 'Remove roles from a user',
+        security: [['sanctum' => []]],
+        tags: ['Admin']
+    )]
+    #[OA\Parameter(
+        name: 'user',
+        description: 'User ID from whom roles will be removed',
+        in: 'path',
+        required: true,
+        example: 12
+    )]
+    #[OA\RequestBody(
+        description: 'Roles removal payload',
+        required: true,
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'roles',
+                    description: 'Array of role IDs to remove',
+                    type: 'array',
+                    items: new OA\Items(type: 'integer', example: 3)
+                )
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 204,
+        description: 'Roles removed successfully'
+    )]
+    #[OA\Response(
+        response: 401,
+        description: 'Unauthenticated',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'message', type: 'string', example: 'Unauthenticated.')
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 422,
+        description: 'Validation errors',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'success', type: 'boolean', example: false),
+                new OA\Property(property: 'message', type: 'string', example: 'Validation errors'),
+                new OA\Property(
+                    property: 'errors',
+                    type: 'object',
+                    example: [
+                        'roles' => [
+                            'The selected roles are invalid.'
+                        ]
+                    ]
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 500,
+        description: 'Server error while removing roles',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'message', type: 'string', example: 'An error occurred while deleting roles.'),
+                new OA\Property(property: 'error', type: 'string', example: 'SQLSTATE[23000]: Integrity constraint violation...')
+            ]
+        )
+    )]
     public function deleteRolesToUser(User $user, UserRoles $request): JsonResponse
     {
         try {
